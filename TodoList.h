11b4c20c029b5c18c1f90b6ce3e9350c935d14c7
Scott Olson <scottolsonofficial@gmail.com>
2020-09-10 21:39:25 +0000

    All code fixed and debuging material removed

--- TodoList.h
+++ TodoList.h
@@ -15,23 +15,24 @@ private:
 	vector<string> tasks;
 public:
     TodoList () {
-        cout << "In Constructor" << endl;
+        //cout << "In Constructor" << endl;
         string line;
-  		ifstream infile ("example.txt");
+  		ifstream infile ("TODOList.txt");
   		if (infile.is_open()) {
     		while ( getline (infile,line) ) {
-      			cout << line << '\n';
+      			//cout << line << '\n';
 				tasks.push_back(line);
     		}
     		infile.close();
   		}
-
-  		else cout << "Unable to open file";
+  		else {
+			cout << "Unable to open file";
+		}
     };
     virtual ~TodoList() {
-        cout << "In Desctructor" << endl;
+        //cout << "In Desctructor" << endl;
         ofstream outfile;
-		outfile.open("example.txt", ofstream::out | ofstream::trunc);
+		outfile.open("TODOList.txt", ofstream::out | ofstream::trunc);
 		for(int i = 0; i <tasks.size(); i++) {
 			outfile << tasks.at(i);
 			if (i + 1 < tasks.size()) {
@@ -42,21 +43,22 @@ public:
     };
 
     void add(string _duedate, string _task) {
-        cout << "In add" << endl;
+        //cout << "In add" << endl;
 		tasks.push_back(_duedate + " " + _task);
+		cout << "Task: " << _task << " -- Was added for " << _duedate << endl;
     }
     int remove(string _task) {
 
-        cout << "In remove" << endl;
-		cout << "tasks.size() = " << tasks.size() << endl;
+        //cout << "In remove" << endl;
+		//cout << "tasks.size() = " << tasks.size() << endl;
 		int returnVal = 0;
 		for(int i = tasks.size() - 1; i >= 0; i--) {
 			if(tasks.at(i).find(_task) == string::npos) {
-				cout << "Task not found in " << i <<endl;
+				//cout << "Task not found in " << i <<endl;
 			}
 			else {
 				tasks.erase(tasks.begin() + i);
-				cout << "Task was located and erased in element " << i << endl;
+				//cout << "Task was located and erased in element " << i << endl;
 				returnVal = 1;
 			}
 		}
@@ -73,7 +75,7 @@ public:
     void printDaysTasks(string _date) {
 
         cout << "Printing all tasks for " << _date << "..." << endl;
-		for(int i = tasks.size() - 1; i >= 0; i--) {
+		for(int i = 0; i < tasks.size(); i++) {
 			if(tasks.at(i).find(_date) != string::npos) {
 				string tmpString;
 				tmpString = tasks.at(i);
--- main.cpp
+++ main.cpp
@@ -8,18 +8,18 @@ using namespace std;
 int main(int argc, char *argv[]) {
     TodoList mylist;
     for (int i = 0; i < argc; i++) {
-        cout << "argv[" << i << "] = " << argv[i] << endl;
+        //cout << "argv[" << i << "] = " << argv[i] << endl;
     }
     string firstArg = argv[1];
     if (firstArg.compare("add") == 0) {
-        cout << "Doing an add" << endl;
+        //cout << "Doing an add" << endl;
         string date = argv[2];
         string task = argv[3];
         mylist.add(date, task);
-        cout << "date " << date << " task " << task << endl;
+        //cout << "date " << date << " task " << task << endl;
     }
 	if (firstArg.compare("remove") == 0) {
-        cout << "Doing a remove" << endl;
+        //cout << "Doing a remove" << endl;
         string task = argv[2];
         if(mylist.remove(task) == 1) {
         	cout << "removed task: " << task << endl;
@@ -29,15 +29,15 @@ int main(int argc, char *argv[]) {
 		}
     }
 	if (firstArg.compare("printList") == 0) {
-        cout << "Doing a print" << endl;
+        //cout << "Doing a print" << endl;
         mylist.printTodoList();
-		cout << "List Printed" << endl;
+		//cout << "List Printed" << endl;
     }
 	if (firstArg.compare("printDay") == 0) {
-        cout << "Doing a printDay" << endl;
+        //cout << "Doing a printDay" << endl;
         string date = argv[2];
 		mylist.printDaysTasks(date);
-		cout << "Printed tasks on day: " << date << endl;
+		//cout << "Printed tasks on day: " << date << endl;
     }
     return 0;
 }
